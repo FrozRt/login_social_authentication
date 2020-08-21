5a64439bdb5b2b1d81ab94dc75795bdea950c61e
FrozRt <chigrin_m@mail.ru>
2020-08-21 23:15:33 +0200

    Add domain settings

--- login_social_authentication/settings.py
+++ login_social_authentication/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = (os.getenv("DEBUG") == "true")
 
-ALLOWED_HOSTS = [os.getenv("HOST"), "127.0.0.1",]
+ALLOWED_HOSTS = [os.getenv("HOST"), "progdude.info", "127.0.0.1",]
 
 # Application definition
 
